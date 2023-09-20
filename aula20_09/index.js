be271e1e9e2b9d2bbc2b30c3418bc7d723be4be7
Luan Simões <luanc179@gmail.com>
2023-09-20 19:59:29 -0300

    aula20_09

--- aula20_09/index.js
+++ aula20_09/index.js
@@ -2,5 +2,12 @@ const clientId = 'f6974b78e2d0494da23391cd8599b3bb';
 const clientSecret = 'cae1c2b6ba8948488df8d97de2bd4c31';
 
 const getToken = async () => {
-
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic "+btoa(clientId+":"+clientSecret)
+        },
+        body: "grant_type=client_credentials"
+    });
 };
